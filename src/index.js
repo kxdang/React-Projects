dfe11b10872c1609573bc7258f9a57a162e03d73
Kien laptop <ikien.dang@gmail.com>
2019-01-17 19:28:59 -0500

    hello

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class App extends React.Component {
-    constructor(props) { //constructor function is always going to be called here in JavaScript
-        super(props); //Remember, app is borrowing functionality from base class and it has go through a setup and uses super reference to parents functions
+class App extends React.Component { 
+    state = {lat: null, errorMessage: ' '}
+
+componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({lat: position.coords.latitude}),
+        (err) => this.setState({errorMessage: err.message})
         
-        // This is the only time we do direct assignment to this.state
-        this.state = { lat: null };
+    );
+}
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setstate!!
-                this.setState({lat: position.coords.latitude});
-
-            
-            },
-            (err) => console.log(err)
-        );
-    }
 
 
     // React says we have to define render!
     render() {
-     
-        return <div>Lattitude: {this.state.lat} </div>;
+     if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>
+     }
+
+     if(!this.state.errorMessage && this.state.lat) {
+         return <div>Lattitude: {this.state.lat} </div>
+     }
+
+     return <div>Loading!</div>;
+
     }
 }
 
