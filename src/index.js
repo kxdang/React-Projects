0cff116edee80cea6bc72d3b4d4f01beb0268500
kxdang <ikien.dang@gmail.com>
2019-01-17 20:42:53 -0500

    Chapter 6-5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component { 
@@ -10,20 +11,21 @@ componentDidMount(){
         (position) => this.setState({lat: position.coords.latitude}),
         (err) => this.setState({errorMessage: err.message})
         
+        
     );
     
 }
 
 
-
     // React says we have to define render!
     render() {
-     if (this.state.errorMessage && !this.state.lat) {
+     if  (this.state.errorMessage && !this.state.lat) {
         return <div>Error: {this.state.errorMessage}</div>
      }
 
-     if(!this.state.errorMessage && this.state.lat) {
-         return <div>Lattitude: {this.state.lat} </div>
+     if (!this.state.errorMessage && this.state.lat) {
+         
+        return <SeasonDisplay lat={this.state.lat}/>
      }
 
      return <div>Loading!</div>;
